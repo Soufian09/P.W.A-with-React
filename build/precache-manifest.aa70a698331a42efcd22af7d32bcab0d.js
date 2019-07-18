34d9d6bb227be8da5befda834bd60e5a9015250e
soufian09 <soufianelhazman@gmail.com>
2019-07-18 21:00:34 +0200

    up

---
+++ build/precache-manifest.aa70a698331a42efcd22af7d32bcab0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16471e14449ae009a6e2c03b857a4082",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c103fbed026f1344415b",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c103fbed026f1344415b",
+    "url": "/static/js/main.ae79b9fd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bienvenue les woods</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p> 1,2, .. 1,2 test pwa</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
